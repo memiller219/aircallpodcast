b9241276c780e1f39fa3538dc7db5c32d7b1c9e5
Michael <memiller219@gmail.com>
2025-08-05 17:45:56 -0700

    test push

--- app/page.js
+++ app/page.js
@@ -1,42 +1,9 @@
-import "../styles/globals.css";
-import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Page from "../components/Page";
-import Navbar from "../components/Navbar";
-import TopNav from "../components/TopNav";
-import SubHeader from "../components/SubHeader";
-import NavItem from "../components/NavItem";
-import HeroSection from "../components/HeroSection";
-import Releases from "../components/Releases";
-import ReleaseCard from "../components/ReleaseCard";
-import LatestArticles from "../components/LatestArticles";
-import Articles from "../components/Articles";
-import Footer from "../components/Footer";
+import Image from "next/image";
 
-const components = {
-  TopNav: TopNav,
-  Navbar: Navbar,
-  SubHeader: SubHeader,
-  NavItem: NavItem,
-  HeroSection: HeroSection,
-  releases: Releases,
-  ReleaseCard: ReleaseCard,
-  articles: Articles,
-  LatestArticles: LatestArticles,
-  footer: Footer,
-  page: Page,
-};
-
-storyblokInit({
-  accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
-  use: [apiPlugin],
-  components,
-  apiOptions: {
-    region: "",
-  },
-});
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      Hello World
+    </div>
+  );
 }
-
-export default MyApp;
